3e82b8a45017bb7bbfd5225749ee23fbd21736f0
Anshul Gupta <hi.anshulg@gmail.com>
2020-06-19 23:30:07 -0700

    update to dynamic port

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ app.post("/", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("server listening on port 3000");
 });
